862e61a7142f5a73988fb75d63ff25f6cf7dc54d
Administrator <Administrator@WIN-20160308JCX>
2017-02-13 16:26:49 +0800

    add a time utility class

---
+++ NtpTime/src/Utils/TimeUtils.java
@@ -0,0 +1,11 @@
+package Utils;
+
+public class TimeUtils {
+	public static final String DEFAULT_DATE_PATTERN="yyyy-MM-dd";
+	public static final String DEFAULT_TIME_PATTERN="HH:mm:ss";
+	public static final String DEFAULT_DATETIME_PATTERN="yyyy-MM-dd HH:mm:ss";
+	
+	public static void main(String[] args) {
+		System.out.println(DEFAULT_DATE_PATTERN);
+	}
+}
