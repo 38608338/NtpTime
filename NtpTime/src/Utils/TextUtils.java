fd5904de6467ec2b1b55b1e0f1f639794c9f4eef
Administrator <Administrator@WIN-20160308JCX>
2017-02-13 16:21:11 +0800

    add a method toLowers

--- NtpTime/src/Utils/TextUtils.java
+++ NtpTime/src/Utils/TextUtils.java
@@ -5,7 +5,12 @@ public class TextUtils {
 		return source.toUpperCase();
 	}
 	
+	public static String toLowers(String source){
+		return source.toLowerCase();
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(toUppers("aBcDxY"));
+		System.out.println(toLowers("aBcDxY"));
 	}
 }
