2e653525e65b40a7a0408e375a4593ccd370fb66
38608338 <38608338@qq.com>
2017-02-13 16:35:42 +0800

    format date use default pattern

--- NtpTime/src/Utils/TimeUtils.java
+++ NtpTime/src/Utils/TimeUtils.java
@@ -1,11 +1,17 @@
 package Utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TimeUtils {
 	public static final String DEFAULT_DATE_PATTERN="yyyy-MM-dd";
 	public static final String DEFAULT_TIME_PATTERN="HH:mm:ss";
 	public static final String DEFAULT_DATETIME_PATTERN="yyyy-MM-dd HH:mm:ss";
 	
 	public static void main(String[] args) {
-		System.out.println(DEFAULT_DATE_PATTERN);
+		System.out.println(DEFAULT_DATETIME_PATTERN);
+		
+		SimpleDateFormat sdf=new SimpleDateFormat(DEFAULT_DATETIME_PATTERN);
+		System.out.println(sdf.format(new Date()));
 	}
 }
