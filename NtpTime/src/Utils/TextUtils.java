88c00671c9534c0ef6296636c01be9eb39f0c412
Administrator <Administrator@WIN-20160308JCX>
2017-02-13 16:18:18 +0800

    add a method toUppers

--- NtpTime/src/Utils/TextUtils.java
+++ NtpTime/src/Utils/TextUtils.java
@@ -1,7 +1,11 @@
 package Utils;
 
 public class TextUtils {
+	public static String toUppers(String source){
+		return source.toUpperCase();
+	}
+	
 	public static void main(String[] args) {
-		
+		System.out.println(toUppers("aBcDxY"));
 	}
 }
