16272ec7f6cd359b1fe4eff66595ff425090f6a4
38608338 <38608338@qq.com>
2017-03-07 19:47:36 +0800

    use log4j print log

--- NtpTime/src/leo/Test.java
+++ NtpTime/src/leo/Test.java
@@ -1,7 +1,24 @@
 package leo;
 
+import org.apache.log4j.Logger;
+
 public class Test {
+	private org.apache.log4j.Logger logger = Logger.getLogger(getClass());
+	
+	public void print(String message){
+		logger.debug(message);
+		logger.info(message);
+		logger.warn(message);
+		logger.error(message);
+	}
 	public static void main(String[] args) {
 		System.err.println("Oops");
+		
+		//log4j:WARN No appenders could be found for logger (leo.Test).
+		//log4j:WARN Please initialize the log4j system properly.
+		//log4j:WARN See http://logging.apache.org/log4j/1.2/faq.html#noconfig for more info.
+
+		Test t=new Test();
+		t.print("yep");
 	}
 }
