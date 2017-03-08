d261b39225dbd22238eaf46c060d23ed46512e2b
leo <38608338@qq.com>
2017-03-08 09:26:07 +0800

    Log4j NDC and MDC demo

--- NtpTime/src/leo/Test.java
+++ NtpTime/src/leo/Test.java
@@ -1,24 +1,50 @@
 package leo;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.MDC;
+import org.apache.log4j.NDC;
 
 public class Test {
 	private org.apache.log4j.Logger logger = Logger.getLogger(getClass());
 	
 	public void print(String message){
+		//NDC.push("ndc");
+		MDC.put("user","SessionID~127.0.0.1~url");
 		logger.debug(message);
+		MDC.put("user","SessionID~192.168.0.3~url");
 		logger.info(message);
+		MDC.put("user","SessionID~192.168.0.4~url");
 		logger.warn(message);
+		MDC.put("user","SessionID~192.168.0.5~url");
 		logger.error(message);
 	}
 	public static void main(String[] args) {
 		System.err.println("Oops");
 		
-		//log4j:WARN No appenders could be found for logger (leo.Test).
-		//log4j:WARN Please initialize the log4j system properly.
-		//log4j:WARN See http://logging.apache.org/log4j/1.2/faq.html#noconfig for more info.
-
 		Test t=new Test();
-		t.print("yep");
+		//t.print("yep");
+		new Thread(t.new MyThread()).start();
+		new Thread(t.new MyThread()).start();
+		new Thread(t.new MyThread()).start();
+		new Thread(t.new MyThread()).start();
+		new Thread(t.new MyThread()).start();
+		new Thread(t.new MyThread()).start();
+	}
+	
+	private class MyThread implements Runnable{
+
+		@Override
+		public void run() {
+			while (true) {
+				print("DN-JACK-30");
+				
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
 	}
 }
