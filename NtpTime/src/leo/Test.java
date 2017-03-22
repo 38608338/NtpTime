8b5e024d6b4fbcbe2b12e78036a9ecb128a7494a
leo <38608338@qq.com>
2017-03-22 11:51:36 +0800

    log4j rootLogger vs logger

--- NtpTime/src/leo/Test.java
+++ NtpTime/src/leo/Test.java
@@ -5,7 +5,12 @@ import org.apache.log4j.MDC;
 import org.apache.log4j.NDC;
 
 public class Test {
+	//log4j.rootLogger д╛хо
 	private org.apache.log4j.Logger logger = Logger.getLogger(getClass());
+	private org.apache.log4j.Logger logger2 = Logger.getLogger("rootLogger");
+	
+	//log4j.logger.thisProject.file
+	private org.apache.log4j.Logger logger3 = Logger.getLogger("thisProject.file");
 	
 	public void print(String message){
 		//NDC.push("ndc");
@@ -17,6 +22,26 @@ public class Test {
 		logger.warn(message);
 		MDC.put("user","SessionID~192.168.0.5~url");
 		logger.error(message);
+	}
+	public void print2(String message){
+		//NDC.push("ndc");
+		MDC.put("user","SessionID~127.0.0.1~url");
+		logger2.debug(message);
+		MDC.put("user","SessionID~192.168.0.3~url");
+		logger2.info(message);
+		MDC.put("user","SessionID~192.168.0.4~url");
+		logger2.warn(message);
+		MDC.put("user","SessionID~192.168.0.5~url");
+		logger2.error(message);
+		
+		MDC.put("user","SessionID~127.0.0.1~url");
+		logger3.debug(message);
+		MDC.put("user","SessionID~192.168.0.3~url");
+		logger3.info(message);
+		MDC.put("user","SessionID~192.168.0.4~url");
+		logger3.warn(message);
+		MDC.put("user","SessionID~192.168.0.5~url");
+		logger3.error(message);
 	}
 	public static void main(String[] args) {
 		System.err.println("Oops");
@@ -37,6 +62,7 @@ public class Test {
 		public void run() {
 			while (true) {
 				print("DN-JACK-30");
+				print2("DN-JACK-60");
 				
 				try {
 					Thread.sleep(500);
